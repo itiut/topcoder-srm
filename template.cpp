cb32746cec72ad66a75df952cb32ab584283c527
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-29 19:57:37 +0900

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class $CLASSNAME$ {
+public:
+   $RC$ $METHODNAME$( $METHODPARMS$ ) {
+
+   }
+};
+
+$BEGINCUT$
+$TESTCODE$
+
+$DEFAULTMAIN$
+$ENDCUT$
