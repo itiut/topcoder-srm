dff015b61d881a7d94a426b75b4d00fb025895ea
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-04-16 16:45:54 +0900

    Modify path for setup files

--- topcoder-arena/setup_arena.sh
+++ topcoder-arena/setup_arena.sh
@@ -5,18 +5,18 @@ apps_dir=$HOME/.local/share/applications
 icons_dir=$HOME/.icons
 
 if [ ! -d $apps_dir ]; then
-    mkdir $apps_dir
+    mkdir -pv $apps_dir
 fi
-cp topcoder-arena.desktop $apps_dir
+cp -iv topcoder-arena.desktop $apps_dir
 
 if [ ! -d $icons_dir ]; then
-    mkdir $icons_dir
+    mkdir -pv $icons_dir
 fi
-cp topcoder-arena.xpm $icons_dir
+cp -iv topcoder-arena.xpm $icons_dir
 
 if [ ! -d $HOME/local/share ]; then
-    mkdir $HOME/local/share -p
+    mkdir -pv $HOME/local/share
 fi
-ln --interactive --symbolic $PWD $HOME/local/share
+ln -isv $PWD $HOME/local/share
 
-cp contestapplet.conf $HOME
+cp -iv contestapplet.conf $HOME
